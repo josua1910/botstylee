f3ffd66f0c3397f6dcaa530a3ce9bf684bbce5b0
josua1910 <73385008+josua1910@users.noreply.github.com>
2021-06-17 13:39:21 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
-  this.sendContact(m.chat, '6282114499086', 'Benniismael', m)
+  // F this.sendContact(m.chat, '082293592104', 'RANDY STORE', m)
+  this.sendContact(m.chat, '082293592104', 'RANDY STORE', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
